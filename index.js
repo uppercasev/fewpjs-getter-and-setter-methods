5e8c45988879b541c2457ce1fa105808d9699781
Vi Tran <contact.vitran@gmail.com>
2021-11-29 21:40:47 +1100

    Finished assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Add your Circle class here
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+    get diameter() {
+        return this.radius * 2;
+    }
+    get circumference() {
+        return Math.PI * this.diameter;
+    }
+    get area() {
+        return Math.PI * (this.radius * this.radius);
+    }
+    set diameter(number) {
+        if (number > 0) {
+            this.radius = number / 2;
+        } else {
+            throw new Error('Diameter cannot be less than 0.');
+        }
+    }
+    set circumference(number) {
+        if (number > 0) {
+            this.radius = number / (Math.PI * 2);
+        } else {
+            throw new Error('Circumference cannot be less than 0.');
+        }
+    }
+    set area(number) {
+        if (number > 0) {
+            this.radius = Math.sqrt(number / Math.PI);
+        } else {
+            throw new Error('Area cannot be less than 0.');
+        }
+    }
+}
